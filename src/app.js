67dc59b83b4242e4397643d6fd4df3e7d08438de
Flipant Mage <muhammadfarrasajid@gmail.com>
2025-12-10 02:07:54 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const authRoutes = require('./routes/auth.route');
 const categoryRoutes = require('./routes/category.route');
 const eventRoutes = require('./routes/event.route');
 const ticketRoutes = require('./routes/ticket.route');
 
 const app = express();
+const prisma = require('@prisma/client').PrismaClient;
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
