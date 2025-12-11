ac5c294c28caf3051ba892e75b6f9911cf31d769
Flipant Mage <muhammadfarrasajid@gmail.com>
2025-12-12 00:38:57 +0800

    update penerapan password

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -3,7 +3,15 @@ const Joi = require('joi');
 const registerSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string()
+    .min(6)
+    .pattern(new RegExp('^(?=.*[a-zA-Z])(?=.*[0-9])'))
+    .required()
+    .messages({
+      'string.min': 'Password must be at least 6 characters long',
+      'string.pattern.base': 'Password must contain at least one letter and one number',
+      'any.required': 'Password is required'
+    }),
 });
 
 const loginSchema = Joi.object({
